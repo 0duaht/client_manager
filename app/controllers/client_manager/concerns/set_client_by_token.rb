4b93f76899b562382edef0dc9287bfc970dbeb36
Tobi Oduah <tobi.oduah@andela.com>
2019-03-06 00:22:39 +0100

    Remove extra query

--- app/controllers/client_manager/concerns/set_client_by_token.rb
+++ app/controllers/client_manager/concerns/set_client_by_token.rb
@@ -1,41 +1,41 @@
-module ClientManager::Concerns::SetClientByToken
-  extend ActiveSupport::Concern
+module ClientManager
+  module Concerns
+    module SetClientByToken
+      extend ActiveSupport::Concern
 
-  included do
-    before_action :set_client
-    if respond_to?(:helper_method)
-      helper_method :current_client
+      included do
+        before_action :set_client
+        helper_method :current_client if respond_to?(:helper_method)
+      end
+
+      def unauthorized
+        head 401
+      end
+
+      private
+
+      def set_client
+        client_token = request.headers['HTTP_CLIENT_TOKEN'] ||
+                       params['client-token']
+        return unauthorized if client_token.blank?
+
+        begin
+          decoded = JWT.decode(
+            client_token, ClientManager.token_secret, true, algorithm: 'HS256'
+          )
+        rescue JWT::DecodeError
+          return unauthorized
+        end
+
+        @current_client = ClientManager::Client.find_by(
+          id: decoded[0]['client_id']
+        )
+        unauthorized unless @current_client
+      end
+
+      def current_client
+        @current_client
+      end
     end
   end
-
-
-  def unauthorized
-    head 401
-  end
-
-  private
-
-  def set_client
-    client_token = request.headers["HTTP_CLIENT_TOKEN"] || params["client-token"]
-    if !(client_token.blank?)
-      begin
-        decoded = JWT.decode client_token, ClientManager.token_secret, true, {:algorithm => 'HS256'}
-      rescue JWT::DecodeError
-        return unauthorized
-      end
-      if ClientManager::Client.exists?(decoded[0]["client_id"])
-        @current_client = ClientManager::Client.find(decoded[0]["client_id"])
-      else
-        return unauthorized
-      end
-    else
-      return unauthorized
-    end
-  end
-
-
-  def current_client
-    @current_client
-  end
-
 end
